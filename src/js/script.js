76b52333f1db530fabe2334f8b8afa1c70f4a100
Vitaly <bulin.vitaly2013@yandex.kz>
2020-12-27 04:05:57 +0600

    full and adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,46 @@
-function addBackground() {
+'use strict';
 
-    let a = [...document.querySelectorAll('.advantages__item')];
+document.addEventListener('DOMContentLoaded', () => {
+
+    function setMenuBar () {
+
+        let bar = document.createElement('div'),
+            section = document.querySelector('.header');
+
+        bar.classList.add('bar');
+        bar.innerHTML = `<div class="sandwich">
+                            <div class="sandwich__top"></div>
+                            <div class="sandwich__center"></div>
+                            <div class="sandwich__bottom"></div>
+                        </div>`;
+        
+        section.prepend(bar);
+    }
+
+    function showMenu (selector) {
+
+        let bar = document.querySelector('.bar'),
+            block = document.querySelector(selector);
+
+        bar.addEventListener('click', (event) => {
+
+            let et = event.target,
+                icon = document.querySelector('.sandwich'),
+                top = 'sandwich__top',
+                center = 'sandwich__center',
+                bottom = 'sandwich__bottom';
+
+            if (et && et.classList.contains('sandwich') || et.classList.contains(top) || 
+            et.classList.contains(center) || et.classList.contains(bottom)) 
+            {
+
+                icon.classList.toggle('animated');
+                block.classList.toggle('animated');
+                document.body.classList.toggle('noScroll');
+            }
+        });
+    }
     
-    a[0].style.cssText = 'background: url("img/advantages/clock.jpg") center center/cover no-repeat'; 
-    a[1].style.cssText = 'background: url("img/advantages/robohand.jpg") center center/cover no-repeat';
-    a[2].style.cssText = 'background: url("img/advantages/globus.jpg") center center/cover no-repeat';
-    a[3].style.cssText = 'background: url("img/advantages/phone.jpg") center center/cover no-repeat';
-    a[4].style.cssText = 'background: url("img/advantages/jacket.jpg") center center/cover no-repeat';
-    a[5].style.cssText = 'background: url("img/advantages/card.jpg") center center/cover no-repeat';
-}
-addBackground();
+    setMenuBar();
+    showMenu('.promo__menu');
+});
